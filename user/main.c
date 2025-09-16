1ed9bb6d159f097c118ad58c1e9ccc6865f1bac9
RNSH-Z <3127346407@qq.com>
2025-09-16 20:42:45 +0800

    usart_init

--- user/main.c
+++ user/main.c
@@ -2,6 +2,16 @@
 
 int main(void)
 {
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+	USART_InitTypeDef USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 115200;// 波特率
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;// 字长为8位数据格式
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;// 一个停止位
+	USART_InitStructure.USART_Parity = USART_Parity_No;// 无奇偶校验位
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;// 收发模式
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;// 无硬件数据流控制
+	USART_Init(USART1, &USART_InitStructure);
+	USART_Cmd(USART1, ENABLE);
 	while(1)
 	{
 
