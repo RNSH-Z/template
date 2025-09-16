f9845f13c5b7a79b8beeb671e32d943a07e9b292
RNSH-Z <3127346407@qq.com>
2025-09-16 21:17:58 +0800

    GPIO_SET

--- user/main.c
+++ user/main.c
@@ -2,6 +2,7 @@
 
 int main(void)
 {
+	// 使能串口1
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
 	USART_InitTypeDef USART_InitStructure;
 	USART_InitStructure.USART_BaudRate = 115200;// 波特率
@@ -11,7 +12,21 @@ int main(void)
 	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;// 收发模式
 	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;// 无硬件数据流控制
 	USART_Init(USART1, &USART_InitStructure);
-	USART_Cmd(USART1, ENABLE);
+	USART_Cmd(USART1, ENABLE); 
+
+	// 初始化GPIOA的9引脚	TX
+	GPIO_InitTypeDef GPIO_InitStructure;// GPIO初始化结构体
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);// 使能GPIOA时钟
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9; // PA9
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;// 50MHz
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; // 复用推挽输出
+	GPIO_Init(GPIOA, &GPIO_InitStructure);// 初始化PA9
+
+	// 初始化GPIOA的10引脚   RX
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);// 使能GPIOA时钟
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10; // PA10
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; // 上拉输入
+	GPIO_Init(GPIOA, &GPIO_InitStructure);// 初始化PA10
 	while(1)
 	{
 
