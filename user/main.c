610be1e7d0ee6c4ae1b64b4673992cb7dfb170b2
RNSH-Z <3127346407@qq.com>
2025-09-18 18:59:34 +0800

    send_str

--- user/main.c
+++ user/main.c
@@ -1,6 +1,20 @@
 #include "stm32f10x.h"
+#include "stdio.h"
+void My_Uart_Init(void);
+void My_Uart_send_data(USART_TypeDef* USARTx, uint8_t *data, uint16_t len);
 
 int main(void)
+{
+	char str[]={1,2,3,4,5,6,7,8,9,10};
+	My_Uart_Init();
+	My_Uart_send_data(USART1,(uint8_t *)str,10);
+	while(1)
+	{
+
+	}
+}
+
+void My_Uart_Init(void)
 {
 	// 使能串口1
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
@@ -14,19 +28,22 @@ int main(void)
 	USART_Init(USART1, &USART_InitStructure);
 	USART_Cmd(USART1, ENABLE); 
 
-	// 初始化GPIOA的9引脚	TX
-	GPIO_InitTypeDef GPIO_InitStructure;// GPIO初始化结构体
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);// 使能GPIOA时钟
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9; // PA9
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;// 50MHz
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; // 复用推挽输出
-	GPIO_Init(GPIOA, &GPIO_InitStructure);// 初始化PA9
 
-	// 初始化GPIOA的10引脚   RX
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);// 使能GPIOA时钟
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10; // PA10
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; // 上拉输入
-	GPIO_Init(GPIOA, &GPIO_InitStructure);// 初始化PA10
+	 GPIO_InitTypeDef GPIO_InitStructure;// GPIO初始化结构体
+	
+	
+	// // 初始化GPIOA的9引脚	TX
+	// RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);// 使能GPIOA时钟
+	// GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9; // PA9
+	// GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;// 50MHz
+	// GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; // 复用推挽输出
+	// GPIO_Init(GPIOA, &GPIO_InitStructure);// 初始化PA9
+
+	// // 初始化GPIOA的10引脚   RX
+	// RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);// 使能GPIOA时钟
+	// GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10; // PA10
+	// GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; // 上拉输入
+	// GPIO_Init(GPIOA, &GPIO_InitStructure);// 初始化PA10
 
 	//串口重映射
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);// 使能AFIO时钟
@@ -39,8 +56,23 @@ int main(void)
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7; // PB7
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; // 上拉输入
 	GPIO_Init(GPIOB, &GPIO_InitStructure);// 初始化PB7
-	while(1)
-	{
-
-	}
 }
+
+//void fputc(int ch, FILE *f)
+//{
+//	USART_SendData(USART1, (uint8_t) ch);
+//	while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+//	return ch;
+//}
+
+void My_Uart_send_data(USART_TypeDef* USARTx, uint8_t *data, uint16_t len)
+{
+	for(uint16_t i=0; i<len; i++)
+	{
+		while(USART_GetFlagStatus(USARTx, USART_FLAG_TXE) == RESET);//等待发送区空
+		USART_SendData(USARTx, data[i]);
+	}
+	while(USART_GetFlagStatus(USARTx, USART_FLAG_TC) == RESET);//等待发送完成
+}
+
+
